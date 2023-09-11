5b6ca9d7fb38780c38c346f69b02040a6bf30902
elenazahrak <elenazahraa9@gmail.com>
2023-09-11 19:34:01 +0700

    Unit Test 2 (Bonus)

--- main/models.py
+++ main/models.py
@@ -4,6 +4,17 @@ from django.db import models
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    amount = models.IntegerField()
+    menu = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
     description = models.TextField()
+
+    def add_stock(self, quantity):
+        self.stock += quantity
+        self.save()
+        return f"Berhasil menambahkan stock sebanyak {quantity}. Jumlah stock {self.menu} saat ini adalah {self.stock}."
+
+    def order(self):
+        self.stock -= 1
+        self.save()
+        return f"Pelanggan berhasil memesan 1 porsi {self.menu}! Jumlah stock {self.menu} saat ini adalah {self.stock}."
--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test import TestCase, Client
+from main.models import Product
 
 # Create your tests here.
 class mainTest(TestCase):
@@ -10,3 +11,23 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+
+class bonusTest(TestCase):
+    def setUp(self):
+        self.paket_satu = Product.objects.create(menu="Paket Ayam Geprek 1 (Level 1-5)", price=15000, stock=20, description="Ayam Geprek (Level 1-5) + Nasi")
+        self.paket_dua = Product.objects.create(menu="Paket Ayam Geprek 3 (Pedas Nangis)", price=21000, stock=5, description="Ayam Geprek (Level MAX) + Nasi + Es Teh Manis")
+
+    def add_stock_test(self):
+        nama_menu = self.paket_satu.menu
+        stock = self.paket_satu.stock
+        stock_added = 3
+        stock += stock_added
+        self.assertEqual(self.add_stock(self, stock_added), f"Berhasil menambahkan stock sebanyak {stock_added}. Jumlah stock {nama_menu} saat ini adalah {stock}.")
+    
+    def order_test(self):
+        nama_menu = self.paket_dua.menu
+        stock = self.paket_dua.stock
+        stock_ordered = 1
+        stock -= stock_ordered
+        self.assertEqual(self.order(self), f"Pelanggan berhasil memesan {stock_ordered} porsi {nama_menu}! Jumlah stock {nama_menu} saat ini adalah {stock}.")
