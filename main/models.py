b7157451b010d4d1b0b3788adc1b9c76864db025
elenazahrak <elenazahraa9@gmail.com>
2023-09-27 07:29:31 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     menu = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
     stock = models.IntegerField(default=0)
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def add_stock(self, quantity):
         self.stock += quantity
